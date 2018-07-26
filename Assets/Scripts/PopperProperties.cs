b357893aa25110bfcb890298abcfb15f22765be5
anand <anand.mudhaliar@gmail.com>
2018-07-27 04:42:46 +0530

    Initial Load

---
+++ Assets/Scripts/PopperProperties.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopperProperties : MonoBehaviour {
+	
+	public enum POPPER_TYPE{
+		POPER_PURPLE
+		,POPER_YELLOW
+		,POPER_BLUE
+	}
+	//Poper default values
+	[SerializeField]
+	 POPPER_TYPE currentPoperType=POPPER_TYPE.POPER_PURPLE;
+	[SerializeField]
+	public int maxHit=1;
+	public GameObject explotion;
+	public GameObject ProjectileParent;
+	// Use this for initialization
+	void Start () {
+		if(currentPoperType==POPPER_TYPE.POPER_PURPLE)
+		{
+			maxHit=1;
+		}
+		else if(currentPoperType==POPPER_TYPE.POPER_YELLOW)
+		{
+			maxHit=2;
+		}
+		else if(currentPoperType==POPPER_TYPE.POPER_BLUE)
+		{
+			maxHit=3;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void setPoperType(POPPER_TYPE setType){
+		currentPoperType=setType;
+	}
+
+	public POPPER_TYPE getpoperType(){
+		return currentPoperType;
+	}
+
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+	public  UIController instance=null;
+
+	private UIController(){}
+
+	public UIController GetInstance(){
+		if(instance==null){
+			instance=new UIController();
+		}
+		return instance;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
